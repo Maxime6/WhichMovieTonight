f292f68f072791d16b8f69c77ff5202ba2fb1d79
Max6me <maxime.tanter@hotmail.fr>
2025-03-28 16:16:07 +0100

    Initial Commit

---
+++ Findy/FindyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindyApp.swift
+//  Findy
+//
+//  Created by Maxime Tanter on 28/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct FindyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
